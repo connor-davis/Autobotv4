d43d0c81ab7ca5f45abd879c1f6e8613f5b5bb5c
Connor Davis <cnnrproton@gmail.com>
2023-10-08 20:27:28 +0200

    Back to normal method for performing silent shot.

--- Autobot/Functions/SilentShot.cs
+++ Autobot/Functions/SilentShot.cs
@@ -16,12 +16,12 @@ public static class SilentShot
         
         Thread.Sleep(configuration.LethalKeyDelay);
         KeyDown(KeyMapper.GetSharpHookKeyCode(configuration.LethalKey));
-        Thread.Sleep(1);
-        KeyUp(KeyMapper.GetSharpHookKeyCode(configuration.LethalKey));
         Thread.Sleep(configuration.WeaponSwapKeyDelay);
         KeyDown(KeyMapper.GetSharpHookKeyCode(configuration.WeaponSwapKey));
-        Thread.Sleep(5);
+        Thread.Sleep(35);
         KeyUp(KeyMapper.GetSharpHookKeyCode(configuration.WeaponSwapKey));
+        Thread.Sleep(25);
+        KeyUp(KeyMapper.GetSharpHookKeyCode(configuration.LethalKey));
     }
 
     private static void KeyDown(KeyCode keyCode)
