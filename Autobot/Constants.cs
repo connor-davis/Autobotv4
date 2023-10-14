8d553d7cd7d039cbee50927f37de286d9a9c2986
Connor Davis <cnnrproton@gmail.com>
2023-10-14 16:14:29 +0200

    Update version to 1.1.2

--- Autobot/Constants.cs
+++ Autobot/Constants.cs
@@ -2,6 +2,6 @@
 
 public static class Constants
 {
-    public const string Version = "1.1.1";
+    public const string Version = "1.1.2";
     public const string CallOfDutyTitle = "Call of Duty\u00ae";
 }
